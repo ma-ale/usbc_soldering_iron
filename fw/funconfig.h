043fbba67e34f596bdd0056fc1cfcbbed03dec8d
Alessandro Mauri <alemauri001@gmail.com>
2026-04-09 17:16:19 +0200

    firmware not working

--- fw/funconfig.h
+++ fw/funconfig.h
@@ -1,5 +1,8 @@
 #ifndef _FUNCONFIG_H
 #define _FUNCONFIG_H
 
-#endif
+#define FUNCONF_USE_DEBUGPRINTF     0 // can only have one printf
+#define FUNCONF_USE_USBPRINTF       1
+#define FUNCONF_DEBUG_HARDFAULT     0
 
+#endif
--- fw/main.c
+++ fw/main.c
@@ -1,7 +1,36 @@
 #include <ch32fun.h>
+#include <stdio.h>
+#include <string.h>
+#include <fsusb.h>
+
+
+// this callback is mandatory when FUNCONF_USE_USBPRINTF is defined,
+// can be empty though
+void handle_usbfs_input(int numbytes, uint8_t *data)
+{
+	if(numbytes == 1) {
+		switch(data[0]) {
+		case 'b':
+			printf("HAHAHA\n");
+			break;
+		}
+	}
+	else {
+		_write(0, (const char*)data, numbytes);
+	}
+}
+
 
 __attribute__((noreturn)) int main(void)
 {
+	SystemInit();
+	funGpioInitAll();
+	USBFSSetup();
+
+	unsigned int count = 0;
 	while (1) {
+		poll_input(); // usb
+		printf("[%d]: Hello From CH32X035\n", count++);
+		Delay_Ms(100);
 	}
 }
